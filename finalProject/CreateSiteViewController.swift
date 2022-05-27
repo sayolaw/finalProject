5931f0ee925700df2dea7aa75b33cc600fd8c067
Olaoluwa Lawal <shayolaw@gmail.com>
2022-05-26 21:38:54 -0400

    allows user to view location and details of newly created user

--- finalProject/CreateSiteViewController.swift
+++ finalProject/CreateSiteViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class CreateSiteViewController: UIViewController {
 
+    @IBOutlet weak var titleInput: UITextField!
+    @IBOutlet weak var latitudeInput: UITextField!
+    @IBOutlet weak var longitudeInpute: UITextField!
+    var site : Site?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createSite" {
+            site = Site(image: "Bodrum.jpg", name: titleInput.text!,latitude: Double(latitudeInput.text!)!,longitude: Double(longitudeInpute.text!)!)
+            
+            print("this is the \(site?.title)")
+                let destinationController = segue.destination as! SiteDetailViewController
+           
+                destinationController.site = site
+//                collectionView.deselectItem(at: indexPaths[0], animated: true)
+            }
+        
+    }
     /*
     // MARK: - Navigation
 
