9f180bdd1ee087beefe8a8b7abdc91db432305c5
Olaoluwa Lawal <shayolaw@gmail.com>
2022-05-28 19:11:40 -0400

    adds two more images to Site entity

--- finalProject/CreateSiteViewController.swift
+++ finalProject/CreateSiteViewController.swift
@@ -14,9 +14,12 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var latitudeInput: UITextField!
     @IBOutlet weak var longitudeInpute: UITextField!
     var firstImage = UIImage(named:"Aydin")
+    var imageArr = [Data]()
+    var count = 0
     
 
     @IBAction func picture(_ sender: Any) {
+        count+=1
         let vc = UIImagePickerController()
         vc.sourceType = .photoLibrary
         vc.allowsEditing = true
@@ -30,7 +33,7 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
         let titleInput = titleInput.text ?? "No title"
         let latitudeInput = Double(latitudeInput.text ?? "0.0") ?? 0.0
         let longitudeInput = Double(longitudeInpute.text ?? "0.0") ?? 0.0
-        appendSite(title: titleInput, latitude: latitudeInput, longitude: longitudeInput,image: saveImage)
+        appendSite(title: titleInput, latitude: latitudeInput, longitude: longitudeInput,image: imageArr)
     }
     var sites = [Site]()
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -58,11 +61,14 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
            
             saveImage = possibleImage.jpegData(compressionQuality: 1.0)!
             print(saveImage)
+            imageArr.append(saveImage)
+            
         }
         else if let possibleImage = info[.originalImage] as? UIImage {
            
             saveImage = possibleImage.jpegData(compressionQuality: 1.0)!
-            print(saveImage)
+            imageArr.append(saveImage)
+            
         }
         else{
             return
@@ -71,17 +77,21 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
         dismiss(animated: true)
     }
     
-    func appendSite(title:String, latitude: Double, longitude: Double, image:Data){
+    func appendSite(title:String, latitude: Double, longitude: Double, image:[Data]){
        
         let site = Site(context: managedContext)
         site.title = title
 
         site.latitude = Double(latitude)
         site.longitude = Double(longitude)
-        site.image = image
+
+        site.image = image[0]
+        site.image1 = image[1]
+        site.image2 = image[2]
 //        site.sayo = "Hello"
         sites.append(site)
-       
+        
+      
         
 
         
@@ -94,8 +104,11 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
             
             siteEntity.setValue(site.title, forKey: "title")
             siteEntity.setValue(site.image, forKey: "image")
+            siteEntity.setValue(site.image1, forKey: "image1")
+            siteEntity.setValue(site.image2, forKey: "image2")
             siteEntity.setValue(site.latitude, forKey: "latitude")
             siteEntity.setValue(site.longitude, forKey: "longitude")
+            
            
             
         }
@@ -117,9 +130,12 @@ class CreateSiteViewController: UIViewController, UIImagePickerControllerDelegat
                     let longitude = result.value(forKey: "longitude") as! Double
                     
                     let image = result.value(forKey: "image") as! Data
+                    let image1 = result.value(forKey: "image1") as! Data
+                    let image2 = result.value(forKey: "image2") as! Data
+                    let imageCollection = [image,image1,image2]
                    
-                   
-                    appendSite(title: title, latitude: latitude, longitude: longitude,image:image)
+                  
+                    appendSite(title: title, latitude: latitude, longitude: longitude,image:imageCollection)
                    
 
                 }
--- finalProject/SiteDetailViewController.swift
+++ finalProject/SiteDetailViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class SiteDetailViewController: UIViewController {
     @IBOutlet weak var siteTitle: UILabel!
     @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
+    
     var site : Site?
     override func viewDidLoad() {
         super.viewDidLoad()
         siteTitle.text = site?.title
         firstImage.image = UIImage(data: (site?.image)!)
+        secondImage.image = UIImage(data: (site?.image1)!)
+        thirdImage.image = UIImage(data: (site?.image2)!)
         // Do any additional setup after loading the view.
     }
     
