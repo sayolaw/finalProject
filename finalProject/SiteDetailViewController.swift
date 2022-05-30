3f4ede7e32ceabb954ba326351553e1a053d828c
Olaoluwa Lawal <shayolaw@gmail.com>
2022-05-30 18:59:09 -0400

    Cleans up code in siteDetailView Controller

--- finalProject/SiteDetailViewController.swift
+++ finalProject/SiteDetailViewController.swift
@@ -29,18 +29,9 @@ class SiteDetailViewController: UIViewController {
                 let destinationController = segue.destination as! MapViewController
            
                 destinationController.site = site
-//                collectionView.deselectItem(at: indexPaths[0], animated: true)
             }
         
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
